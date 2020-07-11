a1703308a78f350bf430df21e22425e04146bf76
Raghib Mirza <realraghibmrz@gmail.com>
2020-07-12 00:41:14 +0100

    jokes

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(window).on('load', function() {
 		Preloder
 	--------------------*/
 	$(".loader").fadeOut(); 
-	$("#preloder").delay(400).fadeOut("slow");
+	$("#preloder").delay(50).fadeOut("slow");
 
 	if($('.portfolios-area').length > 0 ) {
 		var containerEl = document.querySelector('.portfolios-area');
